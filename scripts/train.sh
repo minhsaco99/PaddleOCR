f2e3249fe43d6ee073494fbafc6c748d29351c14
martin <martin@techainer.com>
2024-08-09 15:47:26 +0700

    update train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,5 @@
 LD_LIBRARY_PATH=~/anaconda3/envs/ocr/lib/ python tools/train.py \
                                                 -c=configs/rec/rec_svtrnet_cppd_base_en.yml \
-                                                -o Global.pretrained_model=models/rec_svtr_cppd_base_48_160_en_train/best_model
+                                                -o Global.checkpoints=models/techainer_svtr_cppd_base_fulltext/latest
+                                                # -o Global.pretrained_model=models/rec_svtr_cppd_base_48_160_en_train/best_model
                                             
